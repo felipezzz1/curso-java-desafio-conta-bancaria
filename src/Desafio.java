8b92dcc8625749c5d1f0f293bf5d3ac87f1b8d1f
felipezzz1 <felipezorzo78@gmail.com>
2024-09-30 16:18:39 -0300

    desafio concluido

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Desafio {
     public static void main(String[] args) {
         String nome = "Bruce Wayne";
@@ -9,5 +11,46 @@ public class Desafio {
         System.out.println("Tipo da conta: " + tipoConta);
         System.out.println("Saldo atual: " + saldo);
         System.out.println("\n----------------------------");
+
+        int opcao = 0;
+
+        String menu = """
+                \n
+                ---Digite sua opção---
+                1- Consultar seu saldo
+                2- Transferir valor
+                3- Depositar valor
+                4- Sair
+                
+                """;
+
+        Scanner leitura = new Scanner(System.in);
+
+        while(opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+            if (opcao == 1) {
+                System.out.println("O seu saldo é " + saldo);
+            } else if (opcao == 2) {
+                System.out.println("Qual valor você deseja transferir");
+                double valorTransferencia = leitura.nextDouble();
+                if (valorTransferencia > saldo) {
+                    System.out.println("Você não possui saldo suficiente");
+                } else {
+                    saldo -= valorTransferencia;
+                    System.out.println("Você transferiu " + valorTransferencia);
+                    System.out.println("Seu saldo atual é de " + saldo);
+                }
+            } else if (opcao == 3) {
+                System.out.println("Qual valor você deseja depositar ");
+                double valorDeposito = leitura.nextDouble();
+                saldo += valorDeposito;
+                System.out.println("Seu saldo atual é de " + saldo);
+            } else if (opcao == 4) {
+                System.out.println("Você optou por sair, até mais :)");
+            }else{
+                System.out.println("Opção inválida. Tente novamente");
+            }
+        }
     }
 }
