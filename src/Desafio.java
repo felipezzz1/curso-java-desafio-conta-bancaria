0fc89de01ed4187a6bd99e0b61c5d24d09e09514
felipezzz1 <felipezorzo78@gmail.com>
2024-09-30 15:54:19 -0300

    Primeiros passos, iniciando dados do cliente

---
+++ src/Desafio.java
@@ -0,0 +1,13 @@
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "Bruce Wayne";
+        String tipoConta = "Corrente";
+        double saldo = 2000.00;
+
+        System.out.println("-----------------------------");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo da conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n----------------------------");
+    }
+}
